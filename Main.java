a03ffb83a4abe8e0548ac7c29770075517ca727a
Aub0101 <aubayed.bhatti@study.beds.ac.uk>
2024-08-29 15:34:06 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Variables to store your details
+        String firstName = "Aubayed";
+        String lastName = "Bhatti";
+        String jobRole = "IT Apprentice";
+
+        // Output your details
+        System.out.println("Name: " + firstName);
+        System.out.println("Surname: " + lastName);
+        System.out.println("Job Role: " + jobRole);
+    }
+}
